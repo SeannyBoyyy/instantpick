72205e7f5506646a01b349859e52a277290d4dc5
Seannyboyy <seamdesagun@gmail.com>
2025-12-10 10:38:25 +0800

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ function App() {
             <div className="flex items-center gap-4">
               {/* Company Logo */}
               <img 
-                src="/logo.png" 
+                src="/instantpick/logo.png" 
                 alt="Company Logo" 
                 className="h-10 w-auto object-contain"
               />
